7d2a8dcffe6e54c652878fd1e1dcd96356a51e68
Ross-A2208 <109631531+Ross-A2208@users.noreply.github.com>
2023-10-18 02:34:47 +0200

    Update ExpenseItem.js

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -3,13 +3,9 @@ import { TiDelete } from 'react-icons/ti';
 import { AppContext } from '../context/AppContext';
 
 const ExpenseItem = (props) => {
-    const { dispatch } = useContext(AppContext);
+    const { dispatch, currency } = useContext(AppContext);
 
-    const handleDeleteExpense = () => {
-        dispatch({
-            type: 'DELETE_EXPENSE',
-            payload: props.id,
-        });
+        
     };
 
     const increaseAllocation = (name) => {
@@ -17,22 +13,31 @@ const ExpenseItem = (props) => {
             name: name,
             cost: 10,
         };
-
+    };
         dispatch({
             type: 'ADD_EXPENSE',
             payload: expense
         });
 
-    }
-
+        const DeleteExpense = () => {
+            const expense = {
+                name: delete_expense,
+                cost: -10,
+        }};
+            dispatch({
+                type: 'DELETE_EXPENSE',
+                payload: props.cost,
+            
+            });
     return (
         <tr>
         <td>{props.name}</td>
-        <td>£{props.cost}</td>
-        <td><button onClick={event=> increaseAllocation(props.name)}>+</button></td>
+        <td>{currency}{props.cost}</td>
+        <td><img src="https://pngimg.com/uploads/plus/plus_PNG121.png" alt="-" width="48" height="48" onClick={event=> increaseAllocation(props.name)}></img></td>
+        <td><img src="https://pngimg.com/uploads/minus/minus_PNG60.png" alt="-" width="48" height="48" onClick={event=> DeleteExpense(props.dname)}></img></td>
         <td><TiDelete size='1.5em' onClick={handleDeleteExpense}></TiDelete></td>
         </tr>
-    );
-};
+        );
+
 
 export default ExpenseItem;
