c2d9c1bd4035de0939c71b6a53f90a5c09a70766
Ross-A2208 <109631531+Ross-A2208@users.noreply.github.com>
2023-10-18 02:36:20 +0200

    Update ExpenseTotal.js

--- src/components/ExpenseTotal.js
+++ src/components/ExpenseTotal.js
@@ -1,14 +1,28 @@
-import React, { useContext } from 'react';
+import React, { useContext, dispatch, } from 'react';
 import { AppContext } from '../context/AppContext';
+import Budget from './Budget';
+ 
 const ExpenseTotal = () => {
-    const { expenses } = useContext(AppContext);
-    const totalExpenses = expenses.reduce((total, item) => {
-        return (total += item.cost);
-    }, 0);
-    return (
-        <div className='alert alert-primary'>
-            <span>Spent so far: £{totalExpenses}</span>
-        </div>
-    );
-};
+	const { expenses, currency, val } = useContext(AppContext);
+
+	const totalExpenses = expenses.reduce((total, item) => {
+		return (total += item.cost);
+	}, 0);
+
+	if(val>Budget) {
+		alert("You cannot Spend more than the budget that is allocated!");
+	}
+	else {
+		dispatch({
+			type: 'Expense_Toatal',
+			payload: val,
+		})
+
+	return (
+		<div className='alert alert-primary'>
+			<label style={{ marginLeft: "2rem" }}>{currency}</label> <span>Spent so far: {currency}{totalExpenses}</span>
+		</div>
+	)
+}};
+
 export default ExpenseTotal;
