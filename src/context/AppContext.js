e6294487cdf88ff67d85940838d710d348f8505a
Ross-A2208 <109631531+Ross-A2208@users.noreply.github.com>
2023-10-18 02:37:56 +0200

    Update AppContext.js

--- src/context/AppContext.js
+++ src/context/AppContext.js
@@ -1,7 +1,7 @@
 import React, { createContext, useReducer } from 'react';
 
 // 5. The reducer - this is used to update the state, based on the action
-export const AppReducer = (state, action) => {
+export const AppReducer = (state, action ) => {
     let budget = 0;
     switch (action.type) {
         case 'ADD_EXPENSE':
@@ -13,9 +13,11 @@ export const AppReducer = (state, action) => {
             );
             total_budget = total_budget + action.payload.cost;
             action.type = "DONE";
+
             if(total_budget <= state.budget) {
                 total_budget = 0;
                 state.expenses.map((currentExp)=> {
+
                     if(currentExp.name === action.payload.name) {
                         currentExp.cost = action.payload.cost + currentExp.cost;
                     }
@@ -28,7 +30,7 @@ export const AppReducer = (state, action) => {
                 alert("Cannot increase the allocation! Out of funds");
                 return {
                     ...state
-                }
+                    }
             }
             case 'RED_EXPENSE':
                 const red_expenses = state.expenses.map((currentExp)=> {
@@ -62,11 +64,12 @@ export const AppReducer = (state, action) => {
             state.budget = action.payload;
 
             return {
-                ...state,
-            };
+                ...state,   
+            }
         case 'CHG_CURRENCY':
             action.type = "DONE";
             state.currency = action.payload;
+            
             return {
                 ...state
             }
@@ -78,7 +81,7 @@ export const AppReducer = (state, action) => {
 
 // 1. Sets the initial state when the app loads
 const initialState = {
-    budget: 2000,
+    budget: 2000, 
     expenses: [
         { id: "Marketing", name: 'Marketing', cost: 50 },
         { id: "Finance", name: 'Finance', cost: 300 },
