1b27acd8c7fdcad14db1367122c31d9c4a6a283b
Ross-A2208 <109631531+Ross-A2208@users.noreply.github.com>
2023-10-19 16:29:29 +0200

    Update ExpenseItem.js

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,11 +1,16 @@
 import React, { useContext } from 'react';
 import { TiDelete } from 'react-icons/ti';
 import { AppContext } from '../context/AppContext';
+import { FaPlusCircle, FaMinusCircle }  from 'react-icons/fa';
 
 const ExpenseItem = (props) => {
-    const { dispatch, currency } = useContext(AppContext);
+    const { dispatch,currency } = useContext(AppContext);
 
-        
+    const handleDeleteExpense = () => {
+        dispatch({
+            type: 'DELETE_EXPENSE',
+            payload: props.id,
+        });
     };
 
     const increaseAllocation = (name) => {
@@ -13,31 +18,34 @@ const ExpenseItem = (props) => {
             name: name,
             cost: 10,
         };
-    };
+
         dispatch({
             type: 'ADD_EXPENSE',
             payload: expense
         });
 
-        const DeleteExpense = () => {
-            const expense = {
-                name: delete_expense,
-                cost: -10,
-        }};
-            dispatch({
-                type: 'DELETE_EXPENSE',
-                payload: props.cost,
-            
-            });
+    }
+    
+    const decreaseAllocation = (name) => {
+        const decrease = {
+            name:name,
+            cost: 10
+        };
+        dispatch({
+            type: 'RED_EXPENSE',
+            payload: decrease
+        })
+    }
+
     return (
         <tr>
         <td>{props.name}</td>
         <td>{currency}{props.cost}</td>
-        <td><img src="https://pngimg.com/uploads/plus/plus_PNG121.png" alt="-" width="48" height="48" onClick={event=> increaseAllocation(props.name)}></img></td>
-        <td><img src="https://pngimg.com/uploads/minus/minus_PNG60.png" alt="-" width="48" height="48" onClick={event=> DeleteExpense(props.dname)}></img></td>
+        <td><FaPlusCircle size='2.2em' color="green" onClick={event=> increaseAllocation(props.name)}>+</FaPlusCircle></td>
+        <td><FaMinusCircle size='2.2em' color="red" onClick={event=> decreaseAllocation(props.name)}>-</FaMinusCircle></td>
         <td><TiDelete size='1.5em' onClick={handleDeleteExpense}></TiDelete></td>
         </tr>
-        );
-
+    );
+};
 
 export default ExpenseItem;
