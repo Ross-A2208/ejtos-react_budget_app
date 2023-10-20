7ddbcdfee31f368fd605aa95038b33caa990faa6
Ross-A2208 <109631531+Ross-A2208@users.noreply.github.com>
2023-10-20 16:20:54 +0200

    Update Currency.js

--- src/components/Currency.js
+++ src/components/Currency.js
@@ -4,13 +4,16 @@ import { AppContext } from '../context/AppContext';
 const Currency = () => {
   const {dispatch, currency } = useContext(AppContext);
 
-	const changeCurrency = (val)=>{
-			dispatch({
-				type: 'CHG_CURRENCY',
-				payload: val,
-			})
-	}
-	
+  const handleSelectChange = event => {
+    //     // get the value of the clicked on option
+    //     // then dispatch an event to update the currency in the store
+       const newCurrency = event.target.value;
+       // BUG: Once I dispatch the event, the rest of the UI does not update
+       dispatch({
+        type: 'CHG_CURRENCY',
+            payload: newCurrency
+        });
+    }
 
   return (
     <div className='alert alert-secondary'>  
@@ -18,7 +21,7 @@ const Currency = () => {
       <select 
         className='btn btn-success dropdown-toggle'
         id='currency'
-        onChange={(event) =>changeCurrency(event)}>Currency ({currency})
+        onChange={(event) =>handleSelectChange(event)}>Currency ({currency})
           <option defaultValue={currency}>Currency ({currency})</option>
           <option value='£'>£ Pound</option>
           <option value='$'>$ Dollar</option>
